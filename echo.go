3a039dc1ed858fb10f51ef3f217e4684d5d47d48
Ling Cheng Nan <nan800130@gmail.com>
2019-05-14 14:52:08 +0800

    Add files via upload

---
+++ echo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//fmt.Printf("hello, world\n")
+	
+	var s,sep string
+	for i:=1 ; i<len(os.Args) ; i++{
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+
+}
